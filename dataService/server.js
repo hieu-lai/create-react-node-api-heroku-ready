88d086148e99718ee1456051199c2fb985e6c8ce
Hieu <hieu@Liams-MBP.gateway>
2019-01-31 10:51:34 +1100

    add path

--- dataService/server.js
+++ dataService/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ app.get('/api/hello', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, '..', 'client/build')));
-  
+
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
